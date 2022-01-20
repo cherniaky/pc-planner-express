c8c229faf3fd80edaffb1bca57d2fa1ac5fde2d2
Yurii <juracherniak@gmail.com>
2022-01-20 14:32:15 +0200

    finished header

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var engine = require("ejs-locals");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -17,10 +17,14 @@ mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
+
+// view engine setup
+app.set("view options", { layout: "layout.ejs" });
+app.engine("ejs", engine);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//pug
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,154 @@
 var express = require('express');
 var router = express.Router();
 
+const async = require('async')
+
+const Category = require("../models/category");
+const ComputerPart = require("../models/computerpart");
+const Manufacturer = require("../models/manufacturer");
+
+let path = require('path');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect("list");
+  res.redirect("main");
 });
 
-router.get('/list',function (req,res,next) {
-   res.render("index", { title: "Express" });
+router.get('/main',function (req,res,next) {
+   res.render("main", { title: "Inventory app" });
 })
+
+// Require controller modules.
+var category_controller = require("../controllers/categoryController");
+var computerpart_controller = require("../controllers/computerpartController");
+var manufacturer_controller = require("../controllers/manufactureController");
+
+/// CATEGORY ROUTES ///
+
+// GET request for creating Category. NOTE This must come before route for id (i.e. display category).
+//router.get("/category/create", category_controller.category_create_get);
+
+// // POST request for creating Category.
+// router.post("/category/create", category_controller.category_create_post);
+
+// // GET request to delete Category.
+// router.get("/category/:id/delete", category_controller.category_delete_get);
+
+// // POST request to delete Category.
+// router.post("/category/:id/delete", category_controller.category_delete_post);
+
+// // GET request to update Category.
+// router.get("/category/:id/update", category_controller.category_update_get);
+
+// // POST request to update Category.
+// router.post("/category/:id/update", category_controller.category_update_post);
+
+// // GET request for one Category.
+// router.get("/category/:id", category_controller.category_detail);
+
+// // GET request for list of all Categories.
+ router.get("/categories", category_controller.category_list);
+
+// /// COMPUTERPART ROUTES ///
+
+// // GET request for creating ComputerPart. NOTE This must come before route for id (i.e. display computerpart).
+// router.get(
+//   "/component/create",
+//   computerpart_controller.computerpart_create_get
+// );
+
+// // POST request for creating ComputerPart.
+// router.post(
+//   "/component/create",
+//   upload.single("part_image"),
+//   computerpart_controller.computerpart_create_post
+// );
+
+// // GET request to delete ComputerPart.
+// router.get(
+//   "/component/:id/delete",
+//   computerpart_controller.computerpart_delete_get
+// );
+
+// // POST request to delete ComputerPart.
+// router.post(
+//   "/component/:id/delete",
+//   computerpart_controller.computerpart_delete_post
+// );
+
+// // GET request to delete ComputerPart image.
+// router.get(
+//   "/component/:id/image/delete",
+//   computerpart_controller.computerpart_delete_image_get
+// );
+
+// // POST request to delete ComputerPart image.
+// router.post(
+//   "/component/:id/image/delete",
+//   computerpart_controller.computerpart_delete_image_post
+// );
+
+// // GET request to update ComputerPart.
+// router.get(
+//   "/component/:id/update",
+//   computerpart_controller.computerpart_update_get
+// );
+
+// // POST request to update ComputerPart.
+// router.post(
+//   "/component/:id/update",
+//   upload.single("part_image"),
+//   computerpart_controller.computerpart_update_post
+// );
+
+// // GET request for one ComputerPart.
+// router.get("/component/:id", computerpart_controller.computerpart_detail);
+
+// // GET request for list of all ComputerParts.
+// router.get("/components", computerpart_controller.computerpart_list);
+
+// /// MANUFACTURER ROUTES ///
+
+// // GET request for creating Manufacturer. NOTE This must come before route for id (i.e. display manufacturer).
+// router.get(
+//   "/manufacturer/create",
+//   manufacturer_controller.manufacturer_create_get
+// );
+
+// // POST request for creating Manufacturer.
+// router.post(
+//   "/manufacturer/create",
+//   manufacturer_controller.manufacturer_create_post
+// );
+
+// // GET request to delete Manufacturer.
+// router.get(
+//   "/manufacturer/:id/delete",
+//   manufacturer_controller.manufacturer_delete_get
+// );
+
+// // POST request to delete Manufacturer.
+// router.post(
+//   "/manufacturer/:id/delete",
+//   manufacturer_controller.manufacturer_delete_post
+// );
+
+// // GET request to update Manufacturer.
+// router.get(
+//   "/manufacturer/:id/update",
+//   manufacturer_controller.manufacturer_update_get
+// );
+
+// // POST request to update Manufacturer.
+// router.post(
+//   "/manufacturer/:id/update",
+//   manufacturer_controller.manufacturer_update_post
+// );
+
+// // GET request for one Manufacturer.
+// router.get("/manufacturer/:id", manufacturer_controller.manufacturer_detail);
+
+// // GET request for list of all Manufactuers.
+// router.get("/manufacturers", manufacturer_controller.manufacturer_list);
 
 module.exports = router;
